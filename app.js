f3ad8257fd4fe990a69cf1b8081457a0a830b533
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2024-06-13 21:47:45 +0200

    restaurant routing

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import options from './src/utils/cors.util.js'
 import authRouter from './src/routes/auth.route.js'
 import userRouter from './src/routes/user.route.js'
 import empLaptopRouter from './src/routes/empLaptop.route.js'
+import restaurantRouter from './src/routes/restaurant.route.js'
 import { ApiResponse } from './src/responses/api.response.js'
 import { serveSwagger, setupSwagger } from './swagger.config.js'
 
@@ -25,6 +26,7 @@ app.use(bodyParser.json())
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/laptop", empLaptopRouter)
+app.use("/api/v1/restaurant", restaurantRouter)
 // app.use("*", (req, res) => {
 //     res.status(404).json(new ApiResponse(false, "Route not found", null))
 // })
---
+++ src/routes/restaurant.route.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import restaurantController from "../controllers/restaurant.controller";
+const restaurantRouter = express.Router()
+
+/**
+ * @swagger
+ * /api/v1/restaurant/add:
+ *   post:
+ *     summary: Registers a new restaurant
+ *     tags: [Restaurant]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstname:
+ *                 type: string
+ *               address:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: restaurant registered successfully
+ *       400:
+ *         description: Bad request
+ */
+
+restaurantRouter.post('/add', restaurantController.registerRestaurant)
+export default restaurantRouter
