1f9eedd1b08a4d2f3eb63efbe29ab15e96de7b66
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2024-06-16 01:19:02 +0200

    bug fix

---
+++ app.js
@@ -0,0 +1,36 @@
+
+import express from 'express'
+import http from "http"
+import { config } from 'dotenv'
+import bodyParser from 'body-parser'
+import { connectDB } from './src/utils/db.util.js'
+import cors from 'cors'
+import options from './src/utils/cors.util.js'
+import authRouter from './src/routes/auth.route.js'
+import userRouter from './src/routes/user.route.js'
+import { ApiResponse } from './src/responses/api.response.js'
+import { serveSwagger, setupSwagger } from './swagger.config.js'
+
+config()
+connectDB()
+
+const app = express()
+const server = http.createServer(app)
+const PORT = process.env.PORT || 5000
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.use(bodyParser.json())
+app.use(express.json())
+app.use("/api/v1/user", userRouter)
+// app.use("*", (req, res) => {
+//     res.status(404).json(new ApiResponse(false, "Route not found", null))
+// })
+
+// Serve Swagger UI
+app.use('/api-docs', serveSwagger, setupSwagger)
+
+server.listen(PORT, (err) => {
+    if (err) throw new Error("[LOG]: Server failed to start")
+    console.log(`[LOG]:Server started successfully on port ${PORT}`)
+})
